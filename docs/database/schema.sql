81feee40ed7662d9f88c39099b59b1ea49b60b0f
Beka Gogichaishvili <bekagogichaishvili2015@gmail.com>
2026-09-01 11:00:01 +0000

    Auto update database schema

--- docs/database/schema.sql
+++ docs/database/schema.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict Hcj0qoIgtFWfb70WldRJGYQQdYtvXfLK1bWfkXSi1RQ68TroC8CURhNAOlOVn3w
+\restrict f5ujdpu8WLfsliR4aFM9mgb9UV3C7BH0r4dri6nMYLSWOCCBeLab0gVQmg7TKX2
 
 -- Dumped from database version 16.15 (Ubuntu 16.15-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.15 (Ubuntu 16.15-0ubuntu0.24.04.1)
@@ -1668,5 +1668,5 @@ ALTER TABLE ONLY public."UserBlocks"
 -- PostgreSQL database dump complete
 --
 
-\unrestrict Hcj0qoIgtFWfb70WldRJGYQQdYtvXfLK1bWfkXSi1RQ68TroC8CURhNAOlOVn3w
+\unrestrict f5ujdpu8WLfsliR4aFM9mgb9UV3C7BH0r4dri6nMYLSWOCCBeLab0gVQmg7TKX2
 
